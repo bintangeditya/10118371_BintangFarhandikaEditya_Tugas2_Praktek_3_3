f45c6ff9fa04fc0e134714049d59199c134b9f87
bintangeditya <bintangfarhandika@gmail.com>
2020-04-09 04:55:32 +0700

    tugas 2 provis praktek 3.3

---
+++ src/provis_3_3/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis_3_3;
+
+/**
+ *
+ * @author binta
+ */
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         MainWindow mWin = new MainWindow();
+         mWin.setSize(500,400);
+         mWin.setVisible(true);
+    }
+    
+}
